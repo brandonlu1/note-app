c908d0a85be73dda9e221596d22bca279925a140
Brandon <brandonbl2021@gmail.com>
2022-12-21 13:44:18 -0500

    react app

---
+++ react/note-app-react/src/App.js
@@ -0,0 +1,10 @@
+import Card from './Components/BlogCard'
+function App() {
+  return (
+    <div>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/note-app-react/src/Components/BlogCard.js
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+
+export default function card(){
+    return(
+        <div>
+            this is a card
+
+        </div>
+    )
+}
